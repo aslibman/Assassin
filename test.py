53bb8896cf420641c934fbd7d917a314b378b71d
Kyler Chase <kylerchase@earthlink.net>
2014-12-22 13:21:08 -0500

    worked on some html

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ from flask import Flask, render_template
 
 app=Flask('__name__')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template("register.html")
 
 if __name__=="__main__":
     app.debug=True
