8395f54bbf0923386a62f7a1c270bc61671794eb
Kyler Chase <kylerchase@earthlink.net>
2014-12-26 21:05:26 -0500

    added search, added page uses

--- test.py
+++ test.py
@@ -18,6 +18,18 @@ def about():
 def home():
     return render_template("home.html")
 
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
+    return render_template("profile.html")
+
+@app.route('/target', methods=['GET', 'POST'])
+def target():
+    return render_template("target.html")
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    return render_template("search.html")
+
 if __name__=="__main__":
     app.debug=True
     app.run()
