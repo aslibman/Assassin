7bbf23300bd8a71f2980235cae3e6502cef54d4a
Alex Libman <kingalex11235@yahoo.com>
2015-01-03 20:39:38 -0500

    login and register system fully functional

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-from databases import register, login, getInfoByUser, getInfoByID
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, session
+from databases import register, authenticate, getInfoByUser, getInfoByID
 from pymongo import MongoClient
 from werkzeug import secure_filename
+from functools import wraps
 import os
 
 c = MongoClient("localhost", 27017)
@@ -17,43 +18,67 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+def loginRequired(func):
+    @wraps(func)
+    def inner(*args,**kwargs):
+        if "username" in session:
+            return func(*args,**kwargs)
+        else:
+            return redirect(url_for("login",next=request.url))
+    return inner
+
+def logout():
+    session.pop("username",None)
 
 @app.route ("/home", methods = ["POST" , "GET"])
+@loginRequired
 def home():
+    if request.method == "POST":
+        if request.form["b"] == "Log Out":
+            logout()
+            return redirect(url_for("login"))
     return render_template("home.html")
 
 @app.route("/",methods = ["POST","GET"])
 @app.route ("/login", methods = ["POST" , "GET"])
-def loginPage():
+def login():
     if request.method == "POST":
+        if request.form["b"] == "Sign Up":
+            return redirect(url_for("registration"))
+        
         user = request.form["username"]
         pword = request.form["password"]
-        l = login(user,pword)
+        l = authenticate(user,pword)
         if l[0]:
-            return redirect(url_for("home"))
+            session["username"] = user
+            next = request.args.get("next",None)
+            if (next != None):
+                return redirect(next)
+            else:
+                return redirect(url_for("home"))
         else:
             return render_template("login.html", message=l[1])
-    return render_template("login.html")
+    return render_template("login.html")	
 	
-@app.route ("/register", methods = ["GET"])
-def display_register():
-	return render_template("register.html")
-	
-@app.route ("/register", methods = ["POST"])
+@app.route ("/register", methods = ["GET", "POST"])
 def registration():
-	x = {}
-	
+    if request.method == "POST":
+        if request.form["b"] == "Log In":
+            return redirect(url_for("login"))
+        
 	first_name = request.form["first_name"]
 	last_name = request.form["last_name"]
 	name = first_name + " " + last_name
-	username = request.form["username"]
+	user = request.form["username"]
 	password = request.form["password"]
 	password2 = request.form["password_confirm"]
-	result, message=register(username,password,password2,name)
-	if result == True:
-		return render_template("home.html")
+	result = register(user,password,password2,name)
+	if result[0]:
+            session["username"] = user
+            return redirect(url_for("home"))
 	else:
-		render_template("register.html",message=message)
+            return render_template("register.html",message=result[1])
+    return render_template("register.html")
 
 @app.route('/index')
 def index():
